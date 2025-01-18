7499b481220c17acb8c821bfb25c0e2c0a3960ad
AMRIT MANASH <58604913+1706017@users.noreply.github.com>
2025-01-18 19:24:23 +0530

    Advanced_Delta_Lake_Features.py

--- Advanced_Delta_Lake_Features.py
+++ Advanced_Delta_Lake_Features.py
@@ -1,5 +1,36 @@
 #To see the changes that has been done till now on to the delta table
 =======================================================================
 %sql
-DESCRIBE HISTORY employees;
+CREATE TABLE student(id INT,name STRING,marks DOUBLE);
+
+%sql
+INSERT INTO student
+VALUES
+(1,'Amrit',75.5),
+(2,'Manash',89.9),
+(3,'Kumar',90),
+(4,'Nasim',89.97),
+(5,'Ayush',100);
+
+
+%sql
+UPDATE student 
+SET marks = marks+10;
+
+
+%sql
+DESCRIBE HISTORY student;
+
+#Using the timestamp to access the specific timestamp delta table
+==================================================================
+%sql
+SELECT * FROM student TIMESTAMP AS OF '2025-01-18T13:47:11.000+00:00';
+
+%sql
+SELECT * FROM student TIMESTAMP AS OF '2025-01-18T13:45:08.000+00:00';
+
+
+
+
+
 
