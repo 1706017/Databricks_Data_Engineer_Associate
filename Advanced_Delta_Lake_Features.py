a344fc2aac829e1f7f1cbd04820e78fe162f10ab
AMRIT MANASH <58604913+1706017@users.noreply.github.com>
2025-01-18 19:29:06 +0530

    Advanced_Delta_Lake_Features.py

--- Advanced_Delta_Lake_Features.py
+++ Advanced_Delta_Lake_Features.py
@@ -30,6 +30,33 @@ SELECT * FROM student TIMESTAMP AS OF '2025-01-18T13:47:11.000+00:00';
 SELECT * FROM student TIMESTAMP AS OF '2025-01-18T13:45:08.000+00:00';
 
 
+#Using the Version of delta lake table to access specifc version of delta lake table
+=======================================================================================
+%sql
+SELECT * FROM student VERSION AS OF 1;
+
+Output:
+id	name	  marks
+1	  Amrit 	75.5
+2	  Manash	89.9
+3	  Kumar	  90
+4	  Nasim 	89.97
+5	  Ayush	  100
+
+%sql
+SELECT * FROM student VERSION AS OF 2;
+
+Output:
+id	name  	marks
+1	  Amrit	  85.5
+2	  Manash	99.9
+3	  Kumar 	100
+4	  Nasim	  99.97
+5	  Ayush 	110
+
+
+
+
 
 
 
