4cf86e9bcc896cfb30813fceea0b7d449455157b
AMRIT MANASH <58604913+1706017@users.noreply.github.com>
2025-01-19 10:54:57 +0530

    Databases and Tables On Databricks.sql

--- Databases and Tables On Databricks.sql
+++ Databases and Tables On Databricks.sql
@@ -12,7 +12,7 @@ VALUES
 DESCRIBE EXTENDED managed_default;
 
 --Output:
-col_name	data_type	comment
+/*col_name	data_type	comment
 width	int	null
 length	int	null
 height	int	null
@@ -29,6 +29,55 @@ Location	dbfs:/user/hive/warehouse/managed_default
 Provider	delta	
 Owner	root	
 Is_managed_location	true	
-Table Properties	[delta.minReaderVersion=1,delta.minWriterVersion=2]	
+Table Properties	[delta.minReaderVersion=1,delta.minWriterVersion=2]	*/
+
+--Creation of External Table using LOCATION keyword
+====================================================
+CREATE TABLE external_default
+(width INT,length INT,height INT)
+LOCATION 'dbfs:/mnt/demo/external_default';
+
+INSERT INTO external_default
+VALUES
+(12,56,89)
+
+DESCRIBE EXTENDED external_default;
+--Output:
+/*col_name	data_type	comment
+width	int	null
+length	int	null
+height	int	null
+		
+# Detailed Table Information		
+Catalog	spark_catalog	
+Database	default	
+Table	external_default	
+Created Time	Sun Jan 19 05:11:55 UTC 2025	
+Last Access	UNKNOWN	
+Created By	Spark 3.3.2	
+Type	EXTERNAL	
+Location	dbfs:/mnt/demo/external_default	
+Provider	delta	
+Owner	root	
+Table Properties	[delta.minReaderVersion=1,delta.minWriterVersion=2]	*/
+
+--Creation of New database apart from default database
+=======================================================
+CREATE DATABASE new_default;
+
+--Creation of Managed Table Inside a new database created Apart from default database in Hive Metastore
+========================================================================================================
+USE new_default;
+
+CREATE TABLE new_managed_table
+(id INT,name STRING);
+
+INSERT INTO new_managed_table
+VALUES
+(1,'Amrit'),
+(2,'Manash');
+
+
+
 
 
